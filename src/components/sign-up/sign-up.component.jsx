9e89bd8b05811289caebdee7c6b4d87f04273637
Ilhyo Sung <sih8189@gmail.com>
2020-02-27 05:57:42 -0500

    fixed typo

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import FromInput from "../form-input/form-input.component";
+import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
 import "./sign-up.styles.scss";
-import FormInput from "../form-input/form-input.component";
 
 class SignUp extends React.Component {
   constructor() {
